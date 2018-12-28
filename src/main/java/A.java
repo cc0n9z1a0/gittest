3b16feac5b870f125f214eca59fcca739dec9631
cc0n9s1rZ1a0 <1790006824@qq.com>
2018-12-28 12:58:37 +0800

    1提交

---
+++ src/main/java/A.java
@@ -0,0 +1,12 @@
+/**
+ * @author 赵聪聪
+ * @description
+ * @date 2018/12/28 12:57:56
+ */
+public class A
+{
+    public static void main(String[] args)
+    {
+        System.out.println("111111");
+    }
+}
